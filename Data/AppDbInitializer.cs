af1c6871fdaf1b6bfd441cbc42fb4fa51e677ccc
Rami Abdul Salam <rame19_83@hotmail.com>
2022-03-14 22:04:40 +0300

    Updated DataBase

---
+++ Data/AppDbInitializer.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCoreWebAPIApplication.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            // Scope with App Services
+            using (var ServiceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                // get db
+                var context = ServiceScope.ServiceProvider.GetService<AppDbContext>();
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(new Models.Books()
+                    {
+                        Title = "first books",
+                        Description = "first book description",
+                        CoverUrl = "Https....",
+                        DateAdded = DateTime.Now.AddDays(-5),
+                        DateRead = DateTime.Now,
+                        Genre = "Comdey",
+                        IsRead = true,
+                        Rate = 6
+                    },
+                        new Models.Books()
+                        {
+                            Title = "second books",
+                            Description = "second book description",
+                            CoverUrl = "Https....",
+                            DateAdded = DateTime.Now.AddDays(-5),
+                            Genre = "Comdey",
+                            IsRead = false
+                        });
+                    context.SaveChanges();
+                
+                }
+            }
+        }
+    }
+}
---
+++ Migrations/20220305195836_InitialDBMigration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FirstCoreWebAPIApplication.Migrations
+{
+    public partial class InitialDBMigration : Migration
+    {
+        // Rami Note
+        // This method is responsible for create
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsRead = table.Column<bool>(type: "bit", nullable: false),
+                    DateRead = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Rate = table.Column<int>(type: "int", nullable: true),
+                    CoverUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateAdded = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        // Rami Note
+        // This method is responsible for remove
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
